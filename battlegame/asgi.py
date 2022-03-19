52cd88d9c1955fb421c189e2976b1152ed277828
Akylbek <ashyrbaew@gmail.com>
2022-03-19 23:09:43 +0600

    Added ASGI

--- battlegame/asgi.py
+++ battlegame/asgi.py
@@ -9,8 +9,31 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.conf.urls import url
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'battlegame.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "battlegame.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+from mainapp.consumers import *
+
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
+
+    # WebSocket chat handler
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            url(r"^battles_create/$", MainappConsumer.as_asgi()),
+            # url(r"^battles_list/$", ListConsumer.as_asgi()),
+            # url(r"^battles/accept/$", AcceptConsumer.as_asgi()),
+            # url(r"^battles_start/$", StartConsumer.as_asgi()),
+            # url(r"^battles_move/$", MoveConsumer.as_asgi()),
+            # url(r"^battles_finish/$", ResultConsumer.as_asgi()),
+        ])
+    ),
+})
